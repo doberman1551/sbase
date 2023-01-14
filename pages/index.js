bc8701ef2ccbcc8b4c4dda08093648b84a406559
dkotsev <dobromir.kotsev@gmail.com>
2023-01-14 22:05:00 +0200

    login

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,69 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
+import supabase from '../utils/supabaseClient'
 
 export default function Home() {
+  const [isAuthenticated, setIsAuthenticated]=useState('false');
+  const [userId,setUserId]=useState('');
+  const [title,setTitle]=useState('');
+  const [url, setUrl]=useState('');
+  useEffect(()=>{
+      const getUser=async()=>{ 
+      const user= await supabase.auth.getUser();
+      console.log(user)
+      if(user){
+        const userId=user.data.user?.id;
+        setIsAuthenticated(true);
+        setUserId(userId);
+      }
+      };
+      getUser()
+  },[])
+// Adding link
+  const addNewLink=async ()=>{
+    try{
+    if(title&&url&&userId){
+    const {data, error }=await supabase.from('onboardLinks').insert({
+      title:title,
+      url:url,
+      user_id:userId
+
+    })
+    .select();
+    if (error) throw error;
+    console.log(data)
+  }}
+  catch (error){
+    console.log("error:", error)
+  }
+  }
   return (
+    <div>
     <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <div className='flex flex-col w-full justify-center items-center mt-4'>
+    {isAuthenticated&&(
+    <>
+    	<label className="font-semibold text-xs mt-3" htmlFor="title">Title</label>
+      <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
+       type="text"
+        name="title"
+        onChange={(e)=>setTitle(e.target.value)}/>
+      <label className="font-semibold text-xs mt-3" htmlFor="url">Url</label>
+      <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
+      type="text" name="url"
+      onChange={(e)=>setUrl(e.target.value)}/>
+      <button type="button"
+      className="inline-flex items-ceenter rounded-md border bg-indigo-600 text-white p-4 my-4"
+      onClick={addNewLink}>
+        Create Onboarding
+      </button>
+      </>
+    )}
+  </div>
+  </div>
   )
 }
--- pages/signup.js
+++ pages/signup.js
@@ -3,12 +3,21 @@ import { useState } from 'react'
 import supabase from '../utils/supabaseClient';
 
 const signup= () => {
- const [email,setEmail]=useState();
- const [password,setPassword]=useState();
- 
- async function signUpWithEmail(){
+ const [email,setEmail]=useState('');
+ const [password,setPassword]=useState('');
+ console.log(email);
+ async function signUpWithEmail(e){
+
+	e.preventDefault()
 	try{
-		const resp=await supabase.auth.signUp({email})
+		
+		const resp=await supabase.auth.signUp({email:email, password:password})
+		const userId=resp.data.user?.id;
+
+		console.log("userId:",userId);
+	
+
+
 	}
 	catch{
 
@@ -19,17 +28,22 @@ const signup= () => {
     <div className="flex flex-col items-center justify-center w-screen h-screen bg-gray-200 text-gray-700">
 
 
-	<h1 className="font-bold text-2xl">Welcome Back :)</h1>
+	<h1 className="font-bold text-2xl">Signup :)</h1>
 	<form className="flex flex-col bg-white rounded shadow-lg p-12 mt-12" action="">
-		<label className="font-semibold text-xs" for="usernameField">Username or Email</label>
-		<input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text"/>
-		<label className="font-semibold text-xs mt-3" for="passwordField">Password</label>
-		<input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"type="password"/>
-		<button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Signup</button>
+		<label className="font-semibold text-xs" htmlFor="usernameField">Username or Email</label>
+		<input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="email" 
+		onChange={(e)=>setEmail(e.target.value)}/>
+		<label className="font-semibold text-xs mt-3" htmlFor="passwordField">Password</label>
+		<input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"type="password"
+		onChange={(e)=>setPassword(e.target.value)}/>
+		<button  onClick={signUpWithEmail} 
+		className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">Sign Up</button>
 		<div className="flex mt-6 justify-center text-xs">
 			<a className="text-blue-400 hover:text-blue-500" href="#">Forgot Password</a>
 			<span className="mx-2 text-gray-300">/</span>
+			
 			<a className="text-blue-400 hover:text-blue-500" href="#">Sign Up</a>
+			
 		</div>
 	</form>
 	
